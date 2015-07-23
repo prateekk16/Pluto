7a65bbbdf762338b4cd75604dee9e0c36036beb6
Prateek Singh <prateekk16@gmail.com>
2015-07-23 15:51:57 +0000

    Status and new navigation

--- app/views/layouts/partials/navbar_profile.blade.php
+++ app/views/layouts/partials/navbar_profile.blade.php
@@ -56,74 +56,50 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+                
+                <div class="col-sm-12 col-md-12">        
+                     <a class="navbar-brand" rel="home" href="#" style="padding: 0;">
+                       <div style="display: inline-block;">
+                              @if($currentUser)
+                                    @if(file_exists('img/users/'.$currentUser->email.'/avatar_small.jpg'))            
+                                       {{ HTML::image('img/users/'.$currentUser->email.'/avatar_small.jpg','avatar',  array('class' => 'avatar')) }}
+                                    @else
+                                     {{ HTML::image('img/blank_small.jpg','avatar',  array('class' => 'avatar')) }}
+                                    @endif
+                                  {{ $currentUser->username }} 
+                              @else
+                                Wasdbox
+                              @endif                           
+                        </div>
+                      </a>        
+                  </div>
+
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="about.html">About</a>
-                    </li>
-                    <li>
-                        <a href="services.html">Services</a>
-                    </li>
-                    <li>
-                        <a href="contact.html">Contact</a>
-                    </li>
+
+                     <li>
+                        <form class="navbar-form" role="search" style="margin-bottom: 0; margin-top: 5px;">
+                              <div class="input-group">
+                                  <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term" style="height: 40px;line-height: : 0px;;">
+                                  <div class="input-group-btn">
+                                      <button class="btn btn-primary" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                                  </div>
+                              </div>
+                        </form>  
+                     </li>   
+
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Portfolio <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a href="portfolio-1-col.html">1 Column Portfolio</a>
-                            </li>
-                            <li>
-                                <a href="portfolio-2-col.html">2 Column Portfolio</a>
-                            </li>
-                            <li>
-                                <a href="portfolio-3-col.html">3 Column Portfolio</a>
-                            </li>
-                            <li>
-                                <a href="portfolio-4-col.html">4 Column Portfolio</a>
-                            </li>
-                            <li>
-                                <a href="portfolio-item.html">Single Portfolio Item</a>
-                            </li>
-                        </ul>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-cog"></i> <b class="caret"></b></a>
+                              <ul class="dropdown-menu">
+                                  <li><a href="#">Settings</a></li> 
+                                  <li><a href="#" data-toggle="modal" data-target=".change-dp-modal">Change DP</a></li>                     
+                                  <li class="divider"></li>                 
+                              </ul>
                     </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Blog <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a href="blog-home-1.html">Blog Home 1</a>
-                            </li>
-                            <li>
-                                <a href="blog-home-2.html">Blog Home 2</a>
-                            </li>
-                            <li>
-                                <a href="blog-post.html">Blog Post</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Other Pages <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a href="full-width.html">Full Width Page</a>
-                            </li>
-                            <li>
-                                <a href="sidebar.html">Sidebar Page</a>
-                            </li>
-                            <li>
-                                <a href="faq.html">FAQ</a>
-                            </li>
-                            <li>
-                                <a href="404.html">404</a>
-                            </li>
-                            <li>
-                                <a href="pricing.html">Pricing Table</a>
-                            </li>
-                        </ul>
-                    </li>
+
+                    <li> <a href="{{ URL::to('logout') }}" <i class="fa fa-power-off"></i></a></li>  
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
@@ -159,6 +135,48 @@
 </div>
 
 
+
+<div class="modal fade change-dp-modal" role="dialog" aria-labelledby="gridSystemModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="gridSystemModalLabel">Change Your Profile Picture...</h4>
+      </div>
+      <div class="modal-body" >
+        <div class="container-fluid" data-provides="fileinput">
+          <div class="row">
+            <div class="col-md-4 col-md-offset-4 fileinput-preview thumbnail" data-trigger="fileinput"> 
+                  @if(file_exists('img/users/'.$currentUser->email.'/avatar_med.jpg'))            
+                     {{ HTML::image('img/users/'.$currentUser->email.'/avatar_med.jpg','avatar') }}
+                  @else
+                   {{ HTML::image('img/blank_med.jpg','avatar') }}
+                  @endif
+                  
+            </div>            
+          </div>
+          <div class="row">
+                  <div class="col-md-4 col-md-offset-4">
+                     {{ Form::open(array('url' => 'profile/uploadAvatar', 'files' => true, 'id'=>'uploadAvatar')) }}                  
+                     <input type="text" name="user" class="hidden" value="{{ $currentUser->email }}"/> 
+                     {{ Form::file('avatar', array('id' => 'choose-avatar', 'class'=> 'hidden')) }}
+                    
+                     <label for="choose-avatar">
+                        <span class="choose-avatar-btn btn btn-primary fileinput-new" style="width :155px;"> Choose a Picture </span>
+                     </label>
+                  </div>
+                
+          </div>  
+        </div>
+      </div>
+      <div class="modal-footer">
+      <button type="submit" class="btn btn-primary" id="uploadAvatar-btn"><span class="uploadAvatar-btn-text"> Upload </span></button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>        
+      </div>
+      {{ Form::close() }}
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
 
 @endif
 
